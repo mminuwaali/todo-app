ff7fb465fa1953205c4225044b039ef74fc2f9bb
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2025-01-31 00:08:55 +0100

    Add TypeScript types for Todo management and UI components

--- src/types/app.d.ts
+++ src/types/app.d.ts
@@ -1,1 +1,38 @@
 /// <reference types="nativewind/types" />
+
+type TodoType = {
+  id: string;
+  title: string;
+  completed: boolean;
+  description: string;
+};
+
+type TodoStoreType = {
+  todos: Todo[];
+  addTodo: (todo: Todo) => void;
+  deleteTodo: (id: string) => void;
+  toggleComplete: (id: string) => void;
+};
+
+type HeaderProps = {
+  title: string;
+  showBackButton?: boolean;
+};
+
+type SearchBarProps = {
+  value: string;
+  placeholder?: string;
+  onChangeText: (text: string) => void;
+};
+
+type TodoCardPropType = TodoType & {
+  onDelete: (id: string) => void;
+  onToggleComplete: (id: string) => void;
+};
+
+type FilterButtonsProps = {
+  filter: FilterType;
+  onFilterChange: (filter: FilterType) => void;
+};
+
+type FilterType = "all" | "completed" | "incomplete";
